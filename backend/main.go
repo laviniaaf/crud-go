5b22d9043e6ce42e3efb45e204eb1a858acb1f01
laviniaaf <236252396+laviniaaf@users.noreply.github.com>
2025-10-10 22:56:34 -0300

    Implement main server with CORS and API routes

---
+++ backend/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+
+	// starts the database and creates the table
+	db = InitDB()
+
+	defer db.Close()
+
+	r := chi.NewRouter()
+
+	// middleware CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		MaxAge:         300,
+	}))
+
+	// API 
+	r.Route("/itens", func(r chi.Router) {
+		r.Post("/", createItem)
+		r.Get("/", readItems)
+		r.Get("/{id}", getItemByID)
+		r.Put("/{id}", updateItem)
+		r.Delete("/{id}", deleteItem)
+	})
+
+	frontendPath := "./frontend"
+	fs := http.FileServer(http.Dir(frontendPath))
+
+	r.Get("/*", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/" {
+			http.ServeFile(w, req, frontendPath+"/index.html")
+			return
+		}
+		fs.ServeHTTP(w, req)
+	})
+
+	log.Println("Server running at http://localhost:8082 <---")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
